4c250674577f6e3437181417884c4d4ed4a724f4
Jennifer Henry <jenhenry1995@gmail.com>
2020-11-17 18:50:56 -0800

    updated routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 //allows to switch screens
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Description from "./pages/Description"
-import Projects from "./pages/Projects"
-import Contact from "./pages/Contact"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Nav from './components/Nav/Nav';
+import Description from './pages/Description';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <Router>
-    <div>
-      {/* proeduces navbar on every page */}
-      <Nav/>
-      <Switch>
-        <Route exact path="/" component = { Description } />
-        <Route exact path="/projects" components = { Projects } />
-        <Route exact path="/contact" components = { Contact } />
-      </Switch>
-    </div>
+      <div>
+        {/* proeduces navbar on every page */}
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Description />
+          </Route>
+          <Route exact path="/projects">
+            <Projects/>
+          </Route>
+          <Route exact path="/contact">
+            <Contact/>
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
-
 
 export default App;
