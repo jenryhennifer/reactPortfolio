57fdf6dc86792a32b0d7cdced18738a3b7ce4961
Jennifer Henry <jenhenry1995@gmail.com>
2020-11-17 18:16:24 -0800

    created switch routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+//allows to switch screens
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Description from "./pages/Description"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
 
 function App() {
   return (
-    <div className="">
+    <Router>
+    <div>
+      {/* proeduces navbar on every page */}
+      <Nav/>
+      <Switch>
+        <Route exact path="/" component = { Description } />
+        <Route exact path="/projects" components = { Projects } />
+        <Route exact path="/contact" components = { Contact } />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
