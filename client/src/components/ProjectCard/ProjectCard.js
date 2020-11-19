0cff82811cf5ff3911948e248ae84227c583ce94
Jennifer Henry <jenhenry1995@gmail.com>
2020-11-18 23:01:18 -0800

    project file

--- client/src/components/ProjectCard/ProjectCard.js
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './style.css';
 
-function ProjectCard(){
-    return(
-        <div></div>
-    )
+export function ProjectCard({ children }) {
+  return (
+    <div className="list-overflow-container">
+      <ul className="list-group">{children}</ul>
+    </div>
+  );
+}
+export function projectItem({ children }) {
+  return <li className="list-group-item">{children}</li>;
 }
